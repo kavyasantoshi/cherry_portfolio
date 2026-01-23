dba24fa1d39fc0ec4f816a99e120fd3c0fa8a59a
Kavyasantoshikorla <23p31a0531@acet.ac.in>
2026-01-23 19:11:57 +0530

    New deployment

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,12 +26,7 @@ const Contact = () => {
           </div>
         </div>
 
-        {/* Google Map */}
         <div className="map-container">
-          {/* <iframe
-            title="Cherries Veg Restaurant Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3816.3539095647616!2d82.23104577492444!3d16.957133283858578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a382952b626832d%3A0xea201af6aa4e193e!2sCHERRIES%20PURE%20VEG%20CAFE!5e0!3m2!1sen!2sus!4v1767454845432!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
         <iframe 
         title="Cherries Veg Restaurant Location"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3816.3539095647616!2d82.23104577492444!3d16.957133283858578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a382952b626832d%3A0xea201af6aa4e193e!2sCHERRIES%20PURE%20VEG%20CAFE!5e0!3m2!1sen!2sus!4v1767454845432!5m2!1sen!2sus" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
